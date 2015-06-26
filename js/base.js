923f264d186550eaf7a0da8970c7468363051b8d
ljwrer <ljwrer@sina.com>
2015-06-26 15:11:15 +0800

    event

--- js/base.js
+++ js/base.js
@@ -4,3 +4,13 @@ var print=function (value){
 var println=function (value){
 	print(value+"<br/>");
 }
+var addEvent=document.addEventListener?function(elem,event,listener,useCapture){
+	elem.addEventListener(event,listener,useCapture);
+}:function(elem,event,listener){
+	elem.attachEvent('on'+event,listener,useCapture);
+}
+var delEvent=document.removeEventListener?function(elem,event,listener,useCapture){
+	elem.removeEventListener(event,listener,useCapture);
+}:function(elem,event,listener){
+	elem.detachEvent('on'+event,listener,useCapture);
+}
